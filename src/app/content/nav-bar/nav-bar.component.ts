fca6d2560683265907c96eb0bda09ac4d959d16b
diedicaeaux4 <thiago_semann@hotmail.com>
2023-07-29 09:05:40 -0300

    Terminanda a tela de profile, e configurado os botões do menu

--- src/app/content/nav-bar/nav-bar.component.ts
+++ src/app/content/nav-bar/nav-bar.component.ts
@@ -10,17 +10,46 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class NavBarComponent implements OnInit {
   user: any = null; // Use o tipo de dado adequado para o usuário
+  isMenuOpen = false;
+  isDesktopView = true; // Adicionamos essa propriedade para controlar a exibição no desktop ou celular
 
-  constructor(private authService: AuthenticationService, private router: Router, private toastr: ToastrService) {}
-
+  constructor(private authService: AuthenticationService, private router: Router, private toastr: ToastrService) {
+    // Adicionamos o evento de redimensionamento (resize) para atualizar a exibição do menu quando a janela for redimensionada
+    window.addEventListener('resize', () => this.checkViewport());
+  }
+  
   ngOnInit(): void {
     this.user = this.authService.getUser(); // use o método apropriado para obter as informações do usuário
+    this.checkViewport();
   }
 
   logout(): void {
     this.authService.logout();
-    this.toastr.error("Deslogado.")
+    this.toastr.error('Deslogado.');
     this.router.navigate(['/login']);
   }
-  
+
+  content(): void {
+    this.router.navigate(['/content']);
+  }
+  profile(): void {
+    this.router.navigate(['/profile']);
+  }
+
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  private checkViewport() {
+    // Obtém a largura atual da janela
+    const width = window.innerWidth;
+
+    // Defina a variável isDesktopView para verdadeiro se a largura for maior que 768px
+    this.isDesktopView = width > 990;
+
+    // Fecha o menu (menu sanduíche) quando a tela for de desktop
+    if (this.isDesktopView) {
+      this.isMenuOpen = false;
+    }
+  }
 }
