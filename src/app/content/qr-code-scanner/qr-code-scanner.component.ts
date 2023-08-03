a99eb66a08649d9b8d9a9df86c5f5eb4abfd441d
diedicaeaux4 <thiago_semann@hotmail.com>
2023-08-02 21:25:42 -0300

    Teste

--- src/app/content/qr-code-scanner/qr-code-scanner.component.ts
+++ src/app/content/qr-code-scanner/qr-code-scanner.component.ts
@@ -30,6 +30,7 @@ export class QrCodeScannerComponent implements OnInit {
     console.log('Result: ', resultString);
     console.log(resultString)
     // Construa a rota com base na URL fornecida
+    
     const route = resultString.replace('https://www.frst.com.br', '');
 
     // Realize o redirecionamento
