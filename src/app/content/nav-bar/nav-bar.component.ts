c43251aa9c4187c50771f3cac554319837355750
diedicaeaux4 <thiago_semann@hotmail.com>
2023-08-15 14:20:36 -0300

    Inserido o botão admin, e colocado o botão ligar/desligar maquinas no admin para funcionar

--- src/app/content/nav-bar/nav-bar.component.ts
+++ src/app/content/nav-bar/nav-bar.component.ts
@@ -35,7 +35,10 @@ export class NavBarComponent implements OnInit {
   profile(): void {
     this.router.navigate(['/profile']);
   }
-
+  admin(): void {
+      this.router.navigate(['/admin']);
+  }
+    
   toggleMenu(): void {
     this.isMenuOpen = !this.isMenuOpen;
   }
