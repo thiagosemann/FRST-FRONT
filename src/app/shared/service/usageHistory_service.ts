0cad5465b865e2b3fa0e1d7c4043de6f9562eb65
Thiago <42595307+thiagosemann@users.noreply.github.com>
2024-01-21 17:15:33 -0300

    Arrumado localhgost

--- src/app/shared/service/usageHistory_service.ts
+++ src/app/shared/service/usageHistory_service.ts
@@ -7,8 +7,8 @@ import { UsageHistory } from '../utilitarios/usageHistory';
   providedIn: 'root'
 })
 export class UsageHistoryService {
-  //private apiUrl = 'https://frst-back-02b607761078.herokuapp.com/usageHistory';
-  private apiUrl = 'http://localhost:80/usageHistory';
+  private apiUrl = 'https://frst-back-02b607761078.herokuapp.com/usageHistory';
+  //private apiUrl = 'http://localhost:80/usageHistory';
 
   constructor(private http: HttpClient) { }
 
